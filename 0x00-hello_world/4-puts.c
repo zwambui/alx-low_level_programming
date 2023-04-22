8769261e4e38fbe84abce11c5c4712a1c2a501e1
zwambui <wambuinganga.wn@gmail.com>
2023-04-22 02:53:35 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include  <stdio.h>
+/**
+ * main - prints using function puts
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}
