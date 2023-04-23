d6f4b6b1711d2a10155e6869551c2e89428f6d3f
zwambui <wambuinganga.wn@gmail.com>
2023-04-23 03:59:15 -0700

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - printing to a standard error
+ * main - printing standard error
  *
  * Return: (1)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\"
-			- %s %s,%d-%d-%d\n", Dora, Korpar, 2015, 10, 19);
+
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
-
-
 }
