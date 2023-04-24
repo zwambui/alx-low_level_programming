d191122bb443bb1ff77e2ab7e0231b8d0098b7dc
zwambui <wambuinganga.wn@gmail.com>
2023-04-24 12:52:04 -0700

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description - printing the alphabet in lowercase
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+
+
+
+
+
+
+
+
+
+}
