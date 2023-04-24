a0266946173cf511bb93a1b93e965711dde9183c
zwambui <wambuinganga.wn@gmail.com>
2023-04-24 13:54:23 -0700

    alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description - printing the alphabet except q and e
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	if (c != 'q' && c != 'e')
+
+	}
+
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
