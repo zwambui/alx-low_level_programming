81a777534f866940f1ab8be1fce52c9be396e1c9
zwambui <wambuinganga.wn@gmail.com>
2023-04-25 12:32:19 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_alphabet - Entry point
  *
@@ -12,8 +11,7 @@ void print_alphabet(void)
 {
 	char ch;
 	for (ch = 'a'; ch <= 'z'; ch++);
+
 	_putchar(ch);
 	_putchar('\n');
-
-
 }
