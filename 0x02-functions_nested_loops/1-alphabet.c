343c0686e878de28d58adddd61471cb8b7681c39
zwambui <wambuinganga.wn@gmail.com>
2023-04-25 09:40:24 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description - prints the alphabet, in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++);
+	_putchar(ch);
+	_putchar('\n');
+	return (0);
+
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define MAIN_HEADER
 
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif 
