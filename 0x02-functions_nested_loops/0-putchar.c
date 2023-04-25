f18e4e341e138a6e24e1aa5b20404419417a4f46
zwambui <wambuinganga.wn@gmail.com>
2023-04-25 08:35:50 -0700

    put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
- * main - Print _putchar
+ * main - Entry point
+ *
  * Return: Always 0
  */
 
 int main(void)
 {
-	_putchar("_putchar");
-
+	_putchar("_putchar\n");
 	return (0);
-
-
 }
