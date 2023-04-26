55bd5cd93502723013157531abddcedfcb1ce20e
zwambui <wambuinganga.wn@gmail.com>
2023-04-26 01:35:44 -0700

    lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description - print  the lowercase alphabet in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch--)
+		putchar(ch);
+
+	putchar('\n');
+	return (0);
+
+
+
+
+
+}
