139af8be44a71bf1abc7acffb1b7a2d53735c961
zwambui <wambuinganga.wn@gmail.com>
2023-04-27 01:57:00 -0700

    is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,16 +2,17 @@
 #include "main.h"
 
 /**
- * isupper - print uppercase letter
+ * _isdigit - print digit 0 through 9
  * @c: char to check
  *
  * Return: 1 or 0
  */
 
-int isupper(int c)
+int _isdigit(int c);
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
+
 }
