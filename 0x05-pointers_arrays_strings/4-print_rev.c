77e7cd208b084fa3ebaeaaca36a6da21327be27a
zwambui <wambuinganga.wn@gmail.com>
2023-05-02 12:24:08 -0700

    prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - prints reverse string
+ * @s: string parameter
+ * Return: Always (0)
+ */
+
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+		{
+			break;
+		}
+
+		count++;
+	}
+
+	for (count--; count >= 0; count--)
+	{
+		putchar(s[count]);
+	}
+
+	putchar('\n');
+}
