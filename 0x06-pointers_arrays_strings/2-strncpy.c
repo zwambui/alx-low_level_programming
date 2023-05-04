0cf471e5fb99fbc9ad62217cdd4a2cbdd1ccbdfa
zwambui <wambuinganga.wn@gmail.com>
2023-05-04 03:54:03 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include  "main.h"
+/**
+ * _strncpy - copies string
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: bytes used
+ * Return: destination pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
+
