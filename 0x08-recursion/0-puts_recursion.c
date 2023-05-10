310193e0ec46e69dc66e957954ac34389ad10a65
zwambui <wambuinganga.wn@gmail.com>
2023-05-10 09:47:40 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - function that prints a string
+ *
+ * return - always 0
+ */
+
+void _puts_recursion(char *s);
+{
+	char *s;
+
+	printf("%c , puts recursion in a string\n");
+
+	return (0);
+
+}
