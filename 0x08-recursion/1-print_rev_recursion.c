0fb3e8d2dc7be39c9ebc899b9e9f823968ee06d0
zwambui <wambuinganga.wn@gmail.com>
2023-05-10 10:50:34 -0700

    reverse recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print reverse string
+ * @s: a reverse string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
