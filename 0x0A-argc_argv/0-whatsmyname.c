97b05514c4f2d67d24e3ed4ab61ad42cc482bd7d
zwambui <wambuinganga.wn@gmail.com>
2023-05-13 02:01:01 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints this programs name
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("argc = %s\n", argv[0]);
+	return (0);
+
+}
