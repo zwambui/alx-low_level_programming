f26cc4c189f566929015cf774097293d6e8e56c5
zwambui <wambuinganga.wn@gmail.com>
2023-05-13 02:35:49 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: integer
+ * @argv: string
+ * Return: 0
+ */
+
+int main(int argc, int *argv[])
+{
+	int i;
+
+	printf("i = 0, %d\n", i++);
+
+	return (0);
+
+}
