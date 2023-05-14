a6ab7afa73d3e74b6a2eb67de0bc4288710e48d2
zwambui <wambuinganga.wn@gmail.com>
2023-05-14 04:23:07 -0700

    minimum change to return

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,53 +1,65 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 /**
- * min_coins - calculate minimum number of coins for change
- * @input: amount of cents
- * Return: minimum number of coins
+ * min_coins - change for an amount of money
+ * argc: integer
+ * arcv: string
+ * @input: minimum coins in change
+ * Return: 0
  */
+
 int min_coins(int input)
 {
-	int counter = 0;
-	int cents[] = {25, 10, 5, 2, 1};
-	int num_coins = sizeof(cents) / sizeof(cents[0]);
+        int i = 0, counter = 0;
+        int cents [] = {25, 10, 5, 2, 1};
 
-	if (input < 0)
-	{
-	printf("0\n");
-	return (1);
-	}
-
-	for (int i = 0; i < num_coins; i++)
-	{
-	while (input >= cents[i])
-	{
-		input -= cents[i];
-		counter++;
-	}
-	}
-
-	return (counter);
+        if (input < 0)
+        {
+        printf("0\n");
+        return (1);
+        }
+        while (1)
+        {
+        if (input >= cents[i])
+        {
+        input = input - cents[i];
+        counter++;
+        }
+        else if (input == 0)
+        {
+        return (counter);
+        }
+        else
+        {
+        i++;
+        }
+        }
+        return (counter);
 }
 
+
 /**
- * main - entry point of the program
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 for success, 1 for error
+ * main - change for an amount of money
+ * @argc: integer
+ * @argv: string
+ * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-	printf("Error\n");
-	return (1);
-	}
+        int input, output;
 
-	int cents = atoi(argv[1]);
-	int num_coins = min_coins(cents);
-
-	printf("%d\n", num_coins);
-
-	return (0);
+        if (argc != 2)
+        {
+        printf("Error\n");
+        return (1);
+        }
+        input = atoi(argv[1]);
+        output = min_coins(input);
+        printf("%d\n", output);
+        return (0);
 }
