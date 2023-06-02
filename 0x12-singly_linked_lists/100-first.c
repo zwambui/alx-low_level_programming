3434c5c2291b6cd84b417934e90c441204031a72
zwambui <wambuinganga.wn@gmail.com>
2023-06-02 09:55:37 -0700

    prints string before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * bfmain - checks the code
+ *
+ * Return: 0
+ */
+
+void __attribute__((constructor)) bfmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
