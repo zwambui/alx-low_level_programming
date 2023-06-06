2b087d419261809d8f4e1372135bea900dbb0b88
zwambui <wambuinganga.wn@gmail.com>
2023-06-06 09:42:10 -0700

    print list int

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_listint - prints content of the string
+ * @h: singly linked list
+ * Return: prints elements of the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int a;
+
+	a = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		a++;
+	}
+
+	return (a);
+
+}
